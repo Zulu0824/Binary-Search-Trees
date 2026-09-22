f8e94c2a2133dd186e8e624f2794fedfe7cec5c2
Zulu0824 <sriharsha.patta@gmail.com>
2026-09-23 03:17:41 +0530

    Minor corrections

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ function printAllOrders(tree, label) {
   console.log("In order:   ", inOrd.join(", "));
 }
 
-// 1. Build BST from random numbers < 100
 const initial = randomArray(15, 100);
 console.log("Initial random array (<100):", initial.join(", "));
 const tree = new Tree(initial);
@@ -32,13 +31,10 @@ const tree = new Tree(initial);
 console.log("\nTree structure:");
 tree.prettyPrint();
 
-// 2. Confirm balanced
 console.log("\nisBalanced() after creation:", tree.isBalanced());
 
-// 3. Print all orders
 printAllOrders(tree, "Traversals (balanced)");
 
-// 4. Unbalance the tree with values > 100
 const bigValues = [150, 200, 175, 300, 250, 400];
 console.log("\nInserting values > 100 to unbalance:", bigValues.join(", "));
 bigValues.forEach((v) => tree.insert(v));
@@ -46,18 +42,12 @@ bigValues.forEach((v) => tree.insert(v));
 console.log("\nTree structure after unbalancing:");
 tree.prettyPrint();
 
-// 5. Confirm unbalanced
 console.log("\nisBalanced() after inserts:", tree.isBalanced());
 
-// 6. Rebalance
 tree.rebalance();
 console.log("\nCalled rebalance()");
 
 console.log("\nTree structure after rebalance:");
 tree.prettyPrint();
-
-// 7. Confirm balanced again
 console.log("\nisBalanced() after rebalance:", tree.isBalanced());
-
-// 8. Print all orders again
 printAllOrders(tree, "Traversals (rebalanced)");
